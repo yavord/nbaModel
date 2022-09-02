8ab58c62f9ee8f9cf6c9d7b18f4a5c0c5e8f19ad
yav-d <yavor.d0@gmail.com>
2022-09-02 07:58:51 -0500

    run roto salary scraper

---
+++ dataloader/roto_salaries.py
@@ -0,0 +1,5 @@
+import scrape.roto_scrape as rs
+
+
+if __name__ == "__main__":
+    rs.get_all_games()
--- dataloader/scrape/roto_scrape.py
+++ dataloader/scrape/roto_scrape.py
@@ -17,10 +17,8 @@ def get_soup(url):
     url_hash=url.replace("/","").replace(":","").replace("?","").replace(".","")
     try:
         with open(BASE_DIR+url_hash, "r", encoding=encoding,errors='ignore') as file:
-            print('ok')
             return BeautifulSoup(file.read(), "html.parser")
     except FileNotFoundError:
-        print(url)
         sleep(randint(2,7))
         html_data = requests.get(url, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}).text
         soup_data = BeautifulSoup(html_data, "html.parser")
@@ -43,23 +41,9 @@ def get_all_games():
             for yr in YEARS:
                 soup: BeautifulSoup = get_soup(BASE_URL.replace('MONTH',mon).replace('DAY',day).replace('YEAR',yr))
                 df = pd.read_csv(io.StringIO(soup.find("pre").text),sep=";")
-                print(df.iloc[-1:])
                 df = df.iloc[:-1]
+                df = df.loc[:,['Date','Name','DK Salary']]
                 all_games.append(df)
     
     all_games_df = pd.concat(all_games)
-    print(all_games_df)
-
-
-# all_games=pd.DataFrame()
-# URL = 'http://rotoguru1.com/cgi-bin/hyday.pl?game=dk&mon=10&day=27&year=2015'
-# URL2 = 'http://rotoguru1.com/cgi-bin/fyday.pl?game=fd&scsv=1&week=1&year=2011'
-# URL3 = 'http://rotoguru1.com/cgi-bin/hyday.pl?game=dk&scsv=1&mon=10&day=27&year=2015'
-# x=get_soup(URL3)
-# print(x.find('pre'))
-# print(x.prettify())
-# df = pd.read_csv(io.StringIO(x.find("pre").text),sep=";")
-# print(df)
-# all_games=pd.concat([all_games,pd.read_csv(io.StringIO(x.find("pre").text),sep=";")])
-# all_games = all_games.iloc[:-1]
-# print(all_games.columns)
+    all_games_df.to_csv('salaries.csv')
